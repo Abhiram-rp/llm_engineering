d43cf913289422e792f880826cced34ed1871ec2
Abhiram-rp <117381802+Abhiram-rp@users.noreply.github.com>
2026-04-06 17:50:01 +0530

    Define system message for FlightAI assistant
    Added a system message to define assistant behavior.

--- Airline AI assistant.py
+++ Airline AI assistant.py
@@ -4,3 +4,10 @@ import gradio as gr
 
 MODEL = "llama3.2"
 openai = OpenAI(base_url='http://localhost:11434/v1', api_key='ollama')
+
+system_message = """
+You are a helpful assistant for an Airline called FlightAI.
+Give short, courteous answers, no more than 1 sentence.
+Always be accurate. If you don't know the answer, say so.
+"""
+
